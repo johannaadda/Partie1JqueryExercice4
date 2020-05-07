55d216dd70384579630caf60e799dbf68ef1a2c7
johannaadda <johannaadda@icloud.com>
2020-05-07 02:23:13 -0700

    Final Ex4

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// base du Jquery a inclure toujours en debut (comme le html), $(document).ready(function() est la version longue , utliser version courte comme ci dessous.
+$(function(){
+// Cibler id text pour modifier l element par hide
+$('#firstText').empty();
+$('#secondText').remove();
+});
+// base du Jquery a inclure toujours en debut (comme le html), $(document).ready(function() est la version longue , utliser version courte comme ci dessous.
+$(function(){
+// Cibler id text pour modifier l element par hide
+$('#SecondText').canceled();
+});
